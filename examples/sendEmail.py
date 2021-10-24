9c736e712fb8f63148fb33be3c676f6e5a2c88fe
Lazy Coder <87596501+AdityaEXP@users.noreply.github.com>
2021-10-24 15:47:57 +0530

    Update sendEmail.py

--- examples/sendEmail.py
+++ examples/sendEmail.py
@@ -1,1 +1,13 @@
+import requests
+from urllib.parse import quote
 
+payload = {
+    'fromEmail' : 'myGmail@gmail.com',
+    'to_email' : 'reciverGmail@gmail.com',
+    'subject' : quote('About Nice Weather!'),
+    'message' : quote('Its such a nice weather lets hang out'),
+    'emailPassword' : 'xsydtdiyiudgiud'
+}
+
+response = requests.get('https://yourdomain.com', params=payload)
+print(response.text)
